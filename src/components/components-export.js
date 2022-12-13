e3cee3c39d4ab6a29465134399c41cb5c815e148
jeth <shaibuabdulsalam56@gmail.com>
2022-12-13 01:29:42 +0100

    initial commit

---
+++ src/components/components-export.js
@@ -0,0 +1,18 @@
+import Navbar from "./Navbar";
+import Features from "./Features";
+// import CardDeal from "./CardDeal";
+// import Business from "./Business";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+
+export {
+    Navbar,
+    Features,
+    // CardDeal,
+    // Business,
+    // Clients,
+    Footer,
+    Testimonials,
+    Hero,
+};
---
+++ src/constants/constant.js
@@ -0,0 +1,28 @@
+
+
+
+// import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox } from "../assets";
+
+export const feature = [
+    {
+        id: "feature-1",
+        icon: star,
+        title: "Rewards",
+        content:
+            "",
+    },
+    {
+        id: "feature-2",
+        icon: shield,
+        title: "100% Secured",
+        content:
+            "We take proactive steps make sure your information and transactions are secure.",
+    },
+    {
+        id: "feature-3",
+        icon: send,
+        title: "Balance Transfer",
+        content:
+            "A balance transfer credit card can save you a lot of money in interest charges.",
+    },
+];
